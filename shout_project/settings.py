85b8db9f1c1a0bed99d0c340e0e08d64e10b0237
suttiphak w <suttiphak.w@gmail.com>
2021-11-13 20:20:08 +0700

    Create shouters urls

--- shout_project/settings.py
+++ shout_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pages',
     'accounts',
+    'shouters',
 ]
 
 MIDDLEWARE = [
--- shout_project/urls.py
+++ shout_project/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('accounts/', include('accounts.urls')),
+    path('shouters/', include('shouters.urls')),
 ]
