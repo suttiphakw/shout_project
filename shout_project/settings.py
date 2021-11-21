e3eb41f64f9bcaf96949919f8a56026552e775cc
suttiphak w <suttiphak.w@gmail.com>
2021-11-22 04:02:52 +0700

    Waiting for consent from facebook

--- shout_project/settings.py
+++ shout_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(1o-psk&4db39^@ae8d@3!#$5hd!my#&o%@x1!%-&-#rx&2tb^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['9ba6-125-26-173-111.ngrok.io', '127.0.0.1']
 
