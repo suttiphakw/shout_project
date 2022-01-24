383fd1ac07cef47fe2297c83df8615198be1f482
suttiphak w <suttiphak.w@gmail.com>
2022-01-24 21:25:31 +0700

    re photo

--- shouters/models.py
+++ shouters/models.py
@@ -190,16 +190,22 @@ def article_pre_save(sender, instance, *args, **kwargs):
                 instance.ig_profile_picture = ig_profile_picture
                 instance.save()
 
-            ig_profile_picture = instance.ig_profile_picture
-            response_text = api__admin_approve_text_message(line_user_id=line_user_id)
-            response_flex = api__admin_approve_flex_message(line_user_id=line_user_id,
-                                                            ig_profile_picture=ig_profile_picture,
-                                                            first_name=first_name,
-                                                            ig_username=ig_username,
-                                                            ig_follower_count=ig_follower_count)
-            response_image = api__admin_approve_image_message(line_user_id=line_user_id)
+                response_text = api__admin_approve_text_message(line_user_id=line_user_id)
+                response_flex = api__admin_approve_flex_message(line_user_id=line_user_id,
+                                                                ig_profile_picture=ig_profile_picture,
+                                                                first_name=first_name,
+                                                                ig_username=ig_username,
+                                                                ig_follower_count=ig_follower_count)
+                response_image = api__admin_approve_image_message(line_user_id=line_user_id)
+            else:
+                response_text = api__admin_approve_text_message(line_user_id=line_user_id)
+                response_flex = api__admin_approve_flex_message(line_user_id=line_user_id,
+                                                                ig_profile_picture=ig_profile_picture,
+                                                                first_name=first_name,
+                                                                ig_username=ig_username,
+                                                                ig_follower_count=ig_follower_count)
+                response_image = api__admin_approve_image_message(line_user_id=line_user_id)
 
-            print(response_text, response_flex, response_image)
 
 def article_post_save(sender, instance, *args, **kwargs):
     if instance.is_approve is True and instance.is_already_approve is False:
