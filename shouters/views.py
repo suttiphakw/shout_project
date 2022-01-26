b46384da04f4d2d5630bb1b3c588284ebd6db526
suttiphak w <suttiphak.w@gmail.com>
2022-01-26 21:02:19 +0700

    your campaign and payment and post reach config

--- shouters/views.py
+++ shouters/views.py
@@ -784,7 +784,10 @@ def register__get_ig_data(request, token):
 
     # Cal Ads Post Reach
     predicted_ad_post_reach = ig_story_view * 3
-    ig_ad_post_reach = (ig_average_post_reach + predicted_ad_post_reach) / 2
+    if ig_average_post_reach < predicted_ad_post_reach:
+        ig_ad_post_reach = ig_average_post_reach
+    else:
+        ig_ad_post_reach = (ig_average_post_reach + predicted_ad_post_reach) / 2
 
     shouter.ig_predicted_ad_post_reach = predicted_ad_post_reach
     shouter.ig_ad_post_reach = ig_ad_post_reach
