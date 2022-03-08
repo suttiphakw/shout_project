deb9603cb045471c53b15dac9343c9026482e4f3
suttiphak w <suttiphak.w@gmail.com>
2022-03-08 16:07:24 +0700

    fixed ig average like

--- shouters/utils/function/fn_engagement_outlier.py
+++ shouters/utils/function/fn_engagement_outlier.py
@@ -4,6 +4,8 @@ def cut(raw_list, id_list):
   sd = statistics.stdev(raw_list)
   ig_average_total_like = statistics.mean(raw_list)
   final_dict = {id_list[idx]: value for idx, value in enumerate(raw_list) if not value < ig_average_total_like - sd and not value > ig_average_total_like + sd}
+  if final_dict:
+    ig_average_total_like = statistics.mean([final_dict[key] for key in final_dict])
   context = {
     'final_dict': final_dict,
     'ig_average_total_like': ig_average_total_like,
