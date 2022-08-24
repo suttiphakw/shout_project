828f91d3d38f549ff4afa600869304eb76a0b703
Suttiphak Wuttimongkolchai <man@Suttiphaks-MacBook-Pro.local>
2022-08-25 00:47:54 +0700

    save media

--- shouters/utils/function/fn_save_photo.py
+++ shouters/utils/function/fn_save_photo.py
@@ -15,5 +15,23 @@ def media_picture(shouter, media_src, counter):
   img_temp = NamedTemporaryFile(delete=True)
   img_temp.write(urlopen(media_src).read())
   img_temp.flush()
-  shouter['ig_media_picture_{}'.format(counter)].save(f"{shouter.first_name}.png", File(img_temp))
+  if counter == 1:
+    shouter.ig_media_picture_1.save(f"{shouter.first_name}.png", File(img_temp))
+  elif counter == 2:
+    shouter.ig_media_picture_2.save(f"{shouter.first_name}.png", File(img_temp))
+  elif counter == 3:
+    shouter.ig_media_picture_3.save(f"{shouter.first_name}.png", File(img_temp))
+  elif counter == 4:
+    shouter.ig_media_picture_4.save(f"{shouter.first_name}.png", File(img_temp))
+  elif counter == 5:
+    shouter.ig_media_picture_5.save(f"{shouter.first_name}.png", File(img_temp))
+  elif counter == 6:
+    shouter.ig_media_picture_6.save(f"{shouter.first_name}.png", File(img_temp))
+  elif counter == 7:
+    shouter.ig_media_picture_7.save(f"{shouter.first_name}.png", File(img_temp))
+  elif counter == 8:
+    shouter.ig_media_picture_8.save(f"{shouter.first_name}.png", File(img_temp))
+  elif counter == 9:
+    shouter.ig_media_picture_9.save(f"{shouter.first_name}.png", File(img_temp))
+
   shouter.save()
